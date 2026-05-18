a25975a97b4ea8670b45c7164b1ca38b50af6ad1
sunilpadwal <sunilpadwal.2015@gmail.com>
2026-05-18 21:54:00 +0530

    Create forex_pairs.py

---
+++ forex_pairs.py
@@ -0,0 +1,12 @@
+FOREX_PAIRS = [
+    "EURUSD=X",
+    "GBPUSD=X",
+    "USDJPY=X",
+    "USDCHF=X",
+    "USDCAD=X",
+    "AUDUSD=X",
+    "NZDUSD=X",
+    "EURJPY=X",
+    "EURGBP=X",
+    "GBPJPY=X"
+]
