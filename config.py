ffc57fd3c286025fac250766610ca0fed71980fc
sunilpadwal <sunilpadwal.2015@gmail.com>
2026-05-21 12:08:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
-CONFIDENCE_THRESHOLD = 80
+import os
 
-ENTRY_DELAY_MINUTES = 1
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY")
 
-SUPPORTED_TRADES = [1, 2]
+# Free API safe settings
+SCAN_LIMIT = 10
 
-TIMEFRAME = "1m"
+# Confidence threshold
+MIN_CONFIDENCE = 75
+
+# Entry buffer
+ENTRY_BUFFER_SECONDS = 20
+
+# Timeframes
+TIMEFRAME_1M = "1min"
+TIMEFRAME_2M = "2min"
