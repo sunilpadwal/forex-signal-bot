51327a620ad4dce9a83793033a7e65618831206f
sunilpadwal <sunilpadwal.2015@gmail.com>
2026-05-21 12:59:23 +0530

    Update forex_pairs.py

--- forex_pairs.py
+++ forex_pairs.py
@@ -2,34 +2,9 @@ FOREX_PAIRS = [
     "EUR/USD",
     "GBP/USD",
     "USD/JPY",
-    "USD/CHF",
-    "USD/CAD",
     "AUD/USD",
-    "NZD/USD",
-
+    "USD/CAD",
     "EUR/JPY",
     "GBP/JPY",
-    "AUD/JPY",
-    "CAD/JPY",
-    "CHF/JPY",
-
     "EUR/GBP",
-    "EUR/CHF",
-    "EUR/AUD",
-    "EUR/NZD",
-    "EUR/CAD",
-
-    "GBP/AUD",
-    "GBP/CAD",
-    "GBP/CHF",
-    "GBP/NZD",
-
-    "AUD/CAD",
-    "AUD/CHF",
-    "AUD/NZD",
-
-    "CAD/CHF",
-    "NZD/JPY",
-    "NZD/CHF",
-    "NZD/CAD"
 ]
